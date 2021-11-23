7b3ef8cf929bf019972268ee1ec92622f08a6b18
seattleowl <32573897+seattleowl@users.noreply.github.com>
2021-11-23 08:15:13 -0800

    the sweater thief

--- OwlCoins/events.js
+++
@@ -1,7 +0,0 @@
-chrome.browserAction.onClicked.addListener(function() {
-    chrome.tabs.executeScript(null, {file: "processRequest.js"});
-});
-
-chrome.runtime.onInstalled.addListener(function() {
-    window.localStorage.owlCoins = "0"
-});
