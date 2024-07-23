a41a818b6b09c8000c990998b4190c64ccaf4ce0
Owen L <32573897+mageowl@users.noreply.github.com>
2024-07-23 10:40:30 -0700

    fill all links

--- docs/js/prompt.js
+++
@@ -1,8 +0,0 @@
-const prompt = document.querySelector("#prompt");
-const input = prompt.querySelector(".input");
-let open = false;
-
-window.addEventListener("keydown", (e) => {
-  if (e.key === "k" && (e.ctrlKey || e.metaKey)) {
-  }
-});
