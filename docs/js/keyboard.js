09b2e0aedd35a00e8c04c4ffa1fe034382678b00
Owen L <32573897+mageowl@users.noreply.github.com>
2024-10-27 11:32:10 -0700

    move page up

--- docs/js/keyboard.js
+++ docs/js/keyboard.js
@@ -1,12 +1,12 @@
 import { goBack } from "./animation.js";
 import { links } from "./links.js";
 import {
-  handleLetter,
-  handleEnterTheme,
-  handleBackspace,
-  hideThemePicker,
-  openThemePicker,
-  pickerOpen,
+    handleLetter,
+    handleEnterTheme,
+    handleBackspace,
+    hideThemePicker,
+    openThemePicker,
+    pickerOpen,
 } from "./themes.js";
 
 export let keyboardSelection = -1;
@@ -15,63 +15,72 @@ export let inputDisabled = false;
 const selector = document.querySelector("div#selector");
 
 export function setKeyboardSelection(v) {
-  keyboardSelection = v;
+    keyboardSelection = v;
 }
 
 export function setInputDisabled(v) {
-  inputDisabled = v;
+    inputDisabled = v;
 }
 
-window.addEventListener("keydown", (e) => {
-  if (inputDisabled) e.preventDefault();
-  else if (
-    pickerOpen &&
-    "abcdefghijklmnopqrstuvwxyz ".includes(e.key.toLowerCase()) &&
-    !(e.ctrlKey || e.metaKey)
-  ) {
-    handleLetter(e.key.toLowerCase());
-  } else if (pickerOpen && e.key === "Enter") {
-    handleEnterTheme();
-  } else if (pickerOpen && e.key === "Backspace") {
-    handleBackspace();
-  } else if (e.key === "ArrowUp" || e.key === "ArrowDown" || e.key === "Tab") {
-    e.preventDefault();
-    if (links.length == 0 || pickerOpen) return;
-
-    let direction = {
-      ArrowUp: -1,
-      ArrowDown: 1,
-      Tab: e.shiftKey ? -1 : 1,
-    }[e.key];
-
-    if (keyboardSelection === -1) {
-      setKeyboardSelection(direction === -1 ? links.length - 1 : 0);
-    } else {
-      setKeyboardSelection(
-        (keyboardSelection + direction + links.length) % links.length,
-      );
-    }
-
+function updateSelection() {
     selector.style.top = `${
-      links[keyboardSelection].getBoundingClientRect().top
+        links[keyboardSelection].getBoundingClientRect().top
     }px`;
-    setTimeout(() => selector.classList.remove("hidden"), 1);
+}
+window.addEventListener("resize", updateSelection);
 
-    e.preventDefault();
-  } else if (e.key === " " || e.key === "Enter") {
-    if (links.length == 0 || keyboardSelection < 0) return;
-    links[keyboardSelection].click();
-  } else if (e.key === "Backspace" && router.path != "/") {
-    goBack();
-  } else if (e.key === "Escape") {
-    if (pickerOpen) {
-      hideThemePicker();
-    } else {
-      keyboardSelection = -1;
-      selector.classList.add("hidden");
+window.addEventListener("keydown", (e) => {
+    if (inputDisabled) e.preventDefault();
+    else if (
+        pickerOpen &&
+        "abcdefghijklmnopqrstuvwxyz ".includes(e.key.toLowerCase()) &&
+        !(e.ctrlKey || e.metaKey)
+    ) {
+        handleLetter(e.key.toLowerCase());
+    } else if (pickerOpen && e.key === "Enter") {
+        handleEnterTheme();
+    } else if (pickerOpen && e.key === "Backspace") {
+        handleBackspace();
+    } else if (
+        e.key === "ArrowUp" ||
+        e.key === "ArrowDown" ||
+        e.key === "Tab"
+    ) {
+        e.preventDefault();
+        if (links.length == 0 || pickerOpen) return;
+
+        let direction = {
+            ArrowUp: -1,
+            ArrowDown: 1,
+            Tab: e.shiftKey ? -1 : 1,
+        }[e.key];
+
+        if (keyboardSelection === -1) {
+            setKeyboardSelection(direction === -1 ? links.length - 1 : 0);
+        } else {
+            setKeyboardSelection(
+                (keyboardSelection + direction + links.length) % links.length,
+            );
+        }
+
+        updateSelection();
+        setTimeout(() => selector.classList.remove("hidden"), 1);
+
+        e.preventDefault();
+    } else if (e.key === " " || e.key === "Enter") {
+        if (links.length == 0 || keyboardSelection < 0) return;
+        links[keyboardSelection].click();
+    } else if (e.key === "Backspace" && router.path != "/") {
+        goBack();
+    } else if (e.key === "Escape") {
+        if (pickerOpen) {
+            hideThemePicker();
+        } else {
+            keyboardSelection = -1;
+            selector.classList.add("hidden");
+        }
+    } else if (e.key === "k" && e.ctrlKey) {
+        openThemePicker();
+        e.preventDefault();
     }
-  } else if (e.key === "k" && e.ctrlKey) {
-    openThemePicker();
-    e.preventDefault();
-  }
 });
--- docs/js/selector.js
+++ docs/js/selector.js
@@ -1,8 +1,8 @@
 import { links } from "./links.js";
 import {
-  inputDisabled,
-  keyboardSelection,
-  setKeyboardSelection,
+    inputDisabled,
+    keyboardSelection,
+    setKeyboardSelection,
 } from "./keyboard.js";
 import { go, goBack } from "./animation.js";
 import { pickerOpen } from "./themes.js";
@@ -10,47 +10,50 @@ import { pickerOpen } from "./themes.js";
 const selector = document.querySelector("div#selector");
 
 function updateSelection() {
-  if (inputDisabled || pickerOpen) return;
+    if (inputDisabled || pickerOpen) return;
 
-  const selected = document.querySelector("a:hover");
+    const selected = document.querySelector("a:hover");
 
-  if (selected != null) {
-    setKeyboardSelection(-1);
+    if (selected != null) {
+        setKeyboardSelection(-1);
 
-    selector.style.top = `${selected.getBoundingClientRect().top}px`;
-    setTimeout(() => selector.classList.remove("hidden"), 1);
-  } else {
-    selector.classList.add("hidden");
-  }
+        selector.style.top = `${selected.getBoundingClientRect().top}px`;
+        setTimeout(() => selector.classList.remove("hidden"), 1);
+    } else {
+        selector.classList.add("hidden");
+    }
 }
 
 function updateLinks() {
-  links.forEach((link) => {
-    link.addEventListener("mouseenter", updateSelection);
-    link.addEventListener("mouseleave", updateSelection);
+    links.forEach((link) => {
+        link.addEventListener("mouseenter", updateSelection);
+        link.addEventListener("mouseleave", updateSelection);
 
-    if (link.origin != window.origin || link.hasAttribute("data-force-reload"))
-      return;
+        if (
+            link.origin != window.origin ||
+            link.hasAttribute("data-force-reload")
+        )
+            return;
 
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
-      go(link.href);
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            go(link.href);
+        });
     });
-  });
 }
 updateLinks();
 
 window.addEventListener("navigate", updateLinks);
 
 window.addEventListener("mousemove", () => {
-  if (keyboardSelection !== -1) {
-    selector.classList.add("hidden");
-  }
+    if (keyboardSelection !== -1) {
+        selector.classList.add("hidden");
+    }
 
-  setKeyboardSelection(-1);
+    setKeyboardSelection(-1);
 });
 
 let pathBack = document.querySelector("h1#path-back");
 pathBack.addEventListener("click", () => {
-  goBack();
+    goBack();
 });
